682d795619da2de7cca0e2e89ef3362c4a8388bc
Samuel Servulo <samuelservulo@gmail.com>
2019-10-27 16:48:22 -0300

    Added param for publish rate update

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -6,14 +6,19 @@ from performance_test.msg import SuperAwesome
 def publisher():
     rospy.init_node("python_publisher", anonymous=True)
 
-    pub = rospy.Publisher("performance_test", SuperAwesome, queue_size=1000)
-    rate = rospy.Rate(10)
+    pub = rospy.Publisher("performance_test", SuperAwesome, queue_size = 1000)
     
+    rate_param = rospy.get_param("/publish_rate")
+    rate = rospy.Rate(rate_param)
+
     while not rospy.is_shutdown():
         msg = SuperAwesome()
-        msg.content = "Sample content"
+        msg.content = str(rospy.Time.now())
         rospy.loginfo(msg)
         pub.publish(msg)
+        if(rate_param != rospy.get_param("/rate_param")):
+            rate_param = rospy.get_param("/rate_param")
+            rate = rospy.Rate(rate_param)
         rate.sleep()
 
 if __name__ == "__main__":
--- src/publisher.cpp
+++ src/publisher.cpp
@@ -1,25 +1,37 @@
 #include "ros/ros.h"
 #include "performance_test/SuperAwesome.h"
 
+int kPublishRate = 10;
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "cpp_publisher");
+
   ros::NodeHandle n;
+  ros::Publisher publisher = n.advertise<performance_test::SuperAwesome>("performance_test", 1000);
 
-  ros::Publisher publisher = n.advertise<performance_test::SuperAwesome>("performance_test", 10);
+  int rate_param = kPublishRate;
+  n.getParam("/publish_rate", rate_param);  
 
-  ros::Rate loop_rate(10);
+  ros::Rate publish_rate(rate_param);
 
   while (ros::ok()) {
     performance_test::SuperAwesome msg;
-    msg.content = "Content sample";
+    msg.content = std::to_string(ros::Time::now().toSec());
     ROS_INFO("%s", msg.content.c_str());
 
     publisher.publish(msg);
 
     ros::spinOnce();
 
-    loop_rate.sleep();
+    int current_rate_param = kPublishRate;
+    n.getParam("/publish_rate", current_rate_param);
+    if(rate_param != current_rate_param) {
+      rate_param = current_rate_param;
+      ros::Rate latest_rate(rate_param);
+      publish_rate = latest_rate;
+    }
+
+    publish_rate.sleep();
   }
   return 0;
 } 
